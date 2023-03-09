90d3c450ec5dc4e4f5bdf601cf0d870ba4854d98
Marcelo <marcsopranzi@gmail.com>
2023-03-09 20:23:05 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.57.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = var.project
+    Onwer       = var.contact
+    ManagedBy   = "Terraform"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "prefix" {
+  type        = string
+  default     = "oncall"
+  description = "description"
+}
+
+variable "project" {
+  default = "on call rota"
+}
+
+variable "contact" {
+  default = "marcsopranzi@gmail.com"
+}
